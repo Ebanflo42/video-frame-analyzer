6f59c42ebe1239a15b784abf062f161f1b9754db
Eben Kadile <eben.cowley42@gmail.com>
2018-09-15 07:01:47 -0400

    external command being used for image resizing

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Parallel.Strategies
 
+import Data.Word
 import Data.STBImage
 import Data.Vector as V
 import Data.List as L
@@ -13,72 +14,31 @@ import System.IO
 
 import Util
 
-type FloatingImage = (Vector (Double, Double, Double), Int, Int)
-
-dropEnd :: Int -> [a] -> [a]
-dropEnd 0 list = list
-dropEnd i list = dropEnd (i - 1) $ L.init list
-
--- | Generate a range of integers in vector form.
-range :: Int -> Int -> V.Vector Int
-range x y
-  | x == y = x `V.cons` empty
-  | x < y  = x `V.cons` (range (x + 1) y)
-  | x > y  = (range x (y + 1)) `V.snoc` y
-
-average :: Floating a => V.Vector (a, a, a) -> (a, a, a)
-average vector =
-  let helper (x,y,z) v =
-       if V.null v then (x,y,z)
-       else
-         let (x',y',z') = V.head v
-         in helper (x + x', y + y', z + z') $ V.tail v
-
-      n = fromIntegral $ V.length vector
-
-      (x,y,z) = helper (0.0, 0.0, 0.0) vector
-
-  in (x/n, y/n, z/n)
-
-readImages :: String -> Int -> IO (V.Vector (Image RGBColor))
-readImages name n =
-  let translate x
-        | x < 10    = "00" L.++ (show x)
-        | x < 100   = '0':(show x)
-        | otherwise = show x
-
-      names = V.map (\s -> "name" L.++ ('-':s) L.++ ".png") $ V.map translate $ 1 `range` n
-
-      unsafeLoad s = loadImage RGB s >>=
+readImages :: Vector String -> IO (Vector (Image RGBColor))
+readImages names =
+  let unsafeLoad s = loadImage RGB s >>=
         \lod -> case lod of
           Right img -> return img
           Left err  -> error err
-
   in V.mapM unsafeLoad names
 
--- | Average groups of NxM pixels. 
-downSample :: Int -> Int -> FloatingImage -> FloatingImage
-downSample n m (pixels, w, h) =
-  if w `mod` n /= 0 || h `mod` m /= 0 then error "Kernel does not fit nicely into image!"
-  else
-    let w' = w `div` n; h' = h `div` m
-        wrng = 0 `range` n; hrng = 0 `range` m
+toPCD :: FloatingImage -> Vector (Double, Double, Double, Double, Double)
+toPCD (pixels, width, height) =
+  let wf   = fromIntegral width
+      hf   = fromIntegral height
+      maxI = width*height
 
-        makeNewPixels result x y =
-          let x1 = x*n; x2 = x1 + n
-              y1 = y*m; y2 = y1 + m
+      helper i =
+        if i == maxI then V.empty
+        else
+          let x = i `mod` width
+              y = i `div` width
+          in ((\(a,b,c) (d,e) -> (a,b,c,d,e)) (pixels ! i) ((fromIntegral x)/wf, (fromIntegral y)/hf)) `cons` helper (i + 1)
 
-              vector =
-                V.foldl1 (V.++) $ V.map (\i -> V.map (\j -> pixels ! (i + j*n)) hrng) wrng
-          in
-            if x == w' then
-              if y == h' then result
-              else makeNewPixels result 0 (y + 1)
-            else makeNewPixels (result `snoc` (average vector)) (x + 1) y
-            
-  in (makeNewPixels V.empty 0 0, w', h')
+  in helper 0
 
 main = do
+
   putStrLn "Input the video's file name (must end with .webm)."
   path <- getLine
   let name = dropEnd 5 path
@@ -91,10 +51,16 @@ main = do
   readProcess "find" ["-type", "f", "-name", "*.png", "-printf", "x"] ""
     >>= writeFile "output.txt"
 
+  let translate x
+        | x < 10    = "00" L.++ (show x)
+        | x < 100   = '0':(show x)
+        | otherwise = show x
+
   readFile "output.txt"
     >>= \s -> do
       let num    = read s :: Int
-      let images = readImages name num
-      return ()
+      let names  = V.map (\s -> "name" L.++ ('-':s) L.++ ".png") $ V.map translate $ 1 `range` num
 
-  return ()
+      V.foldl1 (>>) $ V.map (\name -> readProcess "convert" [name, "-resize", "64x48", name] "") names
+
+      readImages names >> return ()
--- Util.hs
+++ Util.hs
@@ -1,16 +1,61 @@
 module Util where
 
+import Data.Bits
+import Data.Word
 import Data.STBImage
 import Data.Vector.Storable as SV
 import Data.Vector as V
 import Data.List as L
 
-convertVector :: Storable a => SV.Vector a -> V.Vector a
-convertVector vector =
-  if SV.null vector then V.empty
-  else (SV.head vector) `V.cons` (convertVector $ SV.tail vector)
+type FloatingImage = (V.Vector (Double, Double, Double), Int, Int)
 
-convertImage :: Image RGBColor -> (V.Vector (Double, Double, Double), Int, Int)
-convertImage (Image pixels width height) =
+dropEnd :: Int -> [a] -> [a]
+dropEnd 0 list = list
+dropEnd i list = dropEnd (i - 1) $ L.init list
+
+-- | Generate a range of integers in vector form.
+range :: Int -> Int -> V.Vector Int
+range x y
+  | x == y = x `V.cons` V.empty
+  | x < y  = x `V.cons` (range (x + 1) y)
+  | x > y  = (range x (y + 1)) `V.snoc` y
+
+convert2Vector :: Storable a => SV.Vector a -> V.Vector a
+convert2Vector vector =
+  if SV.null vector then V.empty
+  else (SV.head vector) `V.cons` (convert2Vector $ SV.tail vector)
+
+convert2Storable :: Storable a => V.Vector a -> SV.Vector a
+convert2Storable vector =
+  if V.null vector then SV.empty
+  else (V.head vector) `SV.cons` (convert2Storable $ V.tail vector)
+
+img2Floating :: Image RGBColor -> FloatingImage
+img2Floating (Image pixels width height) =
   let convertColor (RGBColor r g b) = (fromIntegral r, fromIntegral g, fromIntegral b)
-  in (V.map convertColor $ convertVector pixels, width, height)
+  in (V.map convertColor $ convert2Vector pixels, width, height)
+
+-- | Average groups of 2x2 pixels. 
+downSample :: Image RGBColor -> Image RGBColor
+downSample (Image pixels w h) =
+  let w' = w `shiftR` 1; h' = h `shiftR` 1
+
+      avg :: RGBColor -> RGBColor -> RGBColor -> RGBColor -> RGBColor
+      avg (RGBColor r1 g1 b1) (RGBColor r2 g2 b2) (RGBColor r3 g3 b3) (RGBColor r4 g4 b4) =
+        RGBColor ((r1 + r2 + r3 + r4) `shiftR` 2) ((g1 + g2 + g3 + g4) `shiftR` 2) ((b1 + b2 + b3 + b4) `shiftR` 2)
+
+      makeNewPixels r x y =
+        let yw     = y*w
+            x1     = x + 1
+            pixel1 = pixels SV.! (x + yw)
+            pixel2 = pixels SV.! (x1 + yw)
+            yw2    = yw + w
+            pixel3 = pixels SV.! (x + yw2)
+            pixel4 = pixels SV.! (x1 + yw2)
+        in
+          if x >= w then
+            if y >= h then SV.empty
+            else makeNewPixels r 0 (y + 2)
+          else makeNewPixels (r `SV.snoc` (avg pixel1 pixel2 pixel3 pixel4)) (x + 2) y
+
+  in Image (makeNewPixels SV.empty 0 0) w' h'
