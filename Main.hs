73539500ca4dc1783cb46672ec7cdd5af75358ec
Eben Kadile <eben.cowley42@gmail.com>
2018-09-15 13:18:54 -0400

    analyzing the differences between frames using bottleneck distance seems to be impractical

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import Data.STBImage
 import Data.Vector as V
 import Data.List as L
 
-import Persistence
+import Persistence hiding (bottleNeckDistance, bottleNeckDistances)
 
 import System.Process
 
@@ -37,6 +37,52 @@ toPCD (pixels, width, height) =
 
   in helper 0
 
+
+-- | The standard (Euclidean) metric between index barcodes.
+indexMetric :: BarCode -> BarCode -> Extended Double
+indexMetric (_, Just _) (_, Nothing)    = Infinity
+indexMetric (_, Nothing) (_, Just _)    = Infinity
+indexMetric (i, Nothing) (j, Nothing)   =
+  Finite $ fromIntegral $ abs $ i - j
+indexMetric (i, Just j) (k, Just l) =
+  let x = i - k; y = j - l
+  in Finite $ sqrt $ fromIntegral $ x*x + y*y
+
+{- |
+  Given a metric, return the maximum of minimum distances bewteen the bar codes.
+  Returns noting if either list of barcodes is empty.
+-}
+bottleNeckDistance :: Ord b => (BarCode -> BarCode -> Extended b) -> [BarCode] -> [BarCode] -> Maybe (Extended b)
+bottleNeckDistance metric diagram1 diagram2
+  | L.null diagram1 = Nothing
+  | L.null diagram2 = Nothing
+  | otherwise       =
+    let first  = L.maximum $ L.map (\p -> L.minimum $ L.map (metric p) diagram2) diagram1
+        second = L.maximum $ L.map (\p -> L.minimum $ L.map (metric p) diagram1) diagram2
+    in Just $ max first second
+
+-- |  Get's all the bottleneck distances; a good way to determine the similarity of the topology of two filtrations.
+bottleNeckDistances :: Ord b => (BarCode -> BarCode -> Extended b) -> [[BarCode]] -> [[BarCode]] -> [Maybe (Extended b)]
+bottleNeckDistances metric diagrams1 diagrams2 =
+  let d = (L.length diagrams1) - (L.length diagrams2)
+  in
+    if d >= 0 then (L.zipWith (bottleNeckDistance metric) diagrams1 diagrams2) L.++ (L.replicate d Nothing)
+    else (L.zipWith (bottleNeckDistance metric) diagrams1 diagrams2) L.++ (L.replicate (-d) Nothing)
+
+scales :: [Float]
+scales =
+  [ 0.039
+  , 0.038
+  , 0.037
+  , 0.036
+  , 0.035
+  , 0.034
+  , 0.033
+  , 0.032
+  , 0.031
+  , 0.03
+  ]
+
 main = do
 
   putStrLn "Input the video's file name (must end with .webm)."
@@ -63,8 +109,8 @@ main = do
   imgs <- readImages (V.map ((L.++) "new") names)
 
   let pcd         = V.map (toPCD . img2Floating) imgs
-  let filtrations = parMapVec (makeVRFiltrationLight [0.3, 0.27, 0.24, 0.21, 0.18, 0.15, 0.12, 0.09, 0.06, 0.03] metric) pcd
+  let filtrations = parMapVec (makeVRFiltrationLight scales metric) pcd
   let barcodes    = parMapVec ((L.take 2) . persistentHomology) filtrations
-  let distances   = parMapWithIndex (\i d -> bottleNeckDistances d (barcodes ! (i + 1))) $ V.tail barcodes
+  let distances   = parMapWithIndex (\i d -> bottleNeckDistances indexMetric d (barcodes ! (i + 1))) $ V.tail barcodes
 
   V.foldl1 (>>) $ V.map (putStrLn . show) distances
